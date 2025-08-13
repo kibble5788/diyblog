b49a96aab72b81c3781afbdefadee00839a855f4
kibble <kibble5788@gmail.com>
2025-08-13 11:32:44 +0000

    df

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,45 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "zh-cn/example" },
+        },
+      ],
+      // 为此网站设置英语为默认语言。
+      defaultLocale: "en",
+      locales: {
+        // 英文文档在 `src/content/docs/en/` 中。
+        en: {
+          label: "English",
+        },
+        // 简体中文文档在 `src/content/docs/zh-cn/` 中。
+        "zh-cn": {
+          label: "简体中文",
+          lang: "zh-CN",
+        },
+      },
+    }),
+  ],
 });
--- src/content/docs/en/example.md
+++ src/content/docs/en/example.md
@@ -1,5 +1,5 @@
 ---
-title: Example Guide
+title: 英文文档
 description: A guide in my new Starlight docs site.
 ---
 
